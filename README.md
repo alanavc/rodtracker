d98778c14ec0f69a35242e95442abb4056e4bf25
Alan Veliz-Cuba <aavcmath@gmail.com>
2014-11-19 10:08:14 -0600

    updated files; included example

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
 Tools to track rod-shaped cells 
 Input: Files with segmented cells
-Output: mat or csv file with lineage information
+Output: csv file with lineage trajectory
+
+
+COLOR CODING:
+yellow=cell that apparently appeared or dissapeared (in current or next frame)
+cyan=cell in current frame with more than 1 candidate in the next frame
+blue=cell in current frame with exactly 1 candidate in the next frame
+magenta=cell in next frame with more than 1 candidate in the current frame
+red=cell in next frame with exactly 1 candidate in the current frame
+
+
+INSTRUCTIONS:
+
+
+1. Set up the correct parameter values
+
+2. Run read_data_fun to load data in matlab
+
+3. Run graph_manualcorrection1_fun to do manual correction of cells (yellow cells only). 
+You can switch between the current and next frame with "right click"
+3.1. To manually declare the lineage of a cell click on a cell in the "current frame"; then click in 
+the two daughter cells (or twice in the same cell if it did not divide).
+3.2. After a manual correction press ENTER to save it.
+3.3. If there are no more yellow cells in the current frame press ENTER to continue
+
+4. Run graph_red_fun to do trivial corrections
+
+5. Run graph_manualcorrection2_fun to check that there are no yellow cells in any current frame
+
+6. Run graph_optimization_fun to get the most likely movement of cells
+
+7. Run getdata_fun to get fluorescence data in mat file
+
+8. Run getdata_csvfile_fun to save data in csv format
+
+
+FORMAT OF CSV FILE:
+
+This file will have the lineage trajectories in the following tree format
+cell1
+cell1
+cell1
+     cell11                 cell12
+     cell11                 cell12
+     cell11                 cell12
+     cell11                 cell12
+           cell111 cell112  cell12
+           cell111 cell112  cell12
+           cell111 cell112        cell121 cell122
+           cell111 cell112        cell121 cell122
---
+++ tracking_script.m
@@ -0,0 +1,61 @@
+%% set up parameters for creating cells
+% folder where segmented files are: e.g. '~/home/data/folder'
+pars.folder='~/GIT_STUFF/rodtracker/images_example';
+% name of files: e.g. if file is 'datainfo001c1.tif', then write 'datainfo'
+%pars.filebasename='dfbwtlaciat37ct';
+pars.filebasename='dfbwtlaciat37ct';
+% number of digits: e.g. if file is 'datainfo001c1.tif', then write 3 (because of the 001)
+pars.numdigits=2;
+% channel index:  e.g. if file is 'datainfo001c1.tif', then write 3 'c1'
+pars.indexchar='c1';
+% file extension:  e.g. if file is 'datainfo001c1.tif', then write 3 'tif'
+pars.fileext='tif';
+% files numbers to be analyzed: for 'datainfo001c1.tif' to 'datainfo048c1.tif' write 1:48
+pars.FILENUMS=1:30;
+% color of bacteria from tif file: color label used in the segmentration (double check!)
+pars.colorbacteria=1;
+% minimal area of bacteria: in case some small areas have the same color as segmented bacteria
+pars.minarea=100;
+% maximal movement of cells (in pixels)
+pars.maxmove=40; 
+% maximal movement of poles after septation
+pars.maxmovediv=(pars.maxmove)/2; %in pixels; for 3 min frames
+% growth rate log(2)/doublingtime
+pars.growth=log(2)/22; 
+% time between frames
+pars.framedist=3; %min
+% error in percentage (increase if segmentation was not good)
+pars.errorlength=.2; 
+%%folder of fluorescence data.
+pars.folderGFP='~/GIT_STUFF/rodtracker/images_example';
+pars.filebasenameGFP='dfbwtlaciat37ct';
+pars.numdigitsGFP=2;
+pars.indexcharGFP='c2';
+pars.fileextGFP='tif';
+pars.FILENUMSGFP=pars.FILENUMS;
+% file where all information will be saved ( .mat extension)
+datafile='example.mat'; 
+
+%% put information into matlab format (automatic)
+read_data_fun(pars,datafile); 
+
+%% correct 1 (manually): fix cells that show up as yellow
+graph_manualcorrection1_fun(datafile,1)
+
+%% make trivial corrections (automatic)
+graph_red_fun(datafile)
+
+%% correct 2 (manually): fix cells that show up as yellow and reduce "cyan/magenta clusters"
+graph_manualcorrection2_fun(datafile,1)
+
+%% optimize movement (automatic)
+graph_optimization_fun(datafile,1)
+
+%% get data
+getdata_fun(datafile,pars)
+%% save data as csv file
+csvfile='example.csv';
+getdata_csvfile_fun(datafile,csvfile)
+%% plot lineage
+M=csvread(csvfile);plot(M)
+
